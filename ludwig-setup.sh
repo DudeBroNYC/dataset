38cc929da9a8798ca6a1aec814a8864c1c3cf8fa
DudeBroNYC <46071179+DudeBroNYC@users.noreply.github.com>
2019-08-02 10:54:03 -0400

    Add files via upload

---
+++ ludwig-setup.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/uber/ludwig.git
+cd ludwig
+pip install virtualenv
+virtualenv -p python3 venv
+source venv/bin/activate
+pip install -r requirements.txt
+python setup.py install
